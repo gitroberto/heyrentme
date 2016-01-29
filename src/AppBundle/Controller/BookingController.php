651ce24afd1861025c07aab621ec2eb9b5a38e30
artur <artur@dantesoft.com>
2016-01-29 15:10:33 +0100

    anfrage fix (width, price)

--- src/AppBundle/Controller/BookingController.php
+++ src/AppBundle/Controller/BookingController.php
@@ -35,7 +35,7 @@ class BookingController extends BaseController {
         $from = DateTime::createFromFormat('Y-m-d\TH:i+', $dateFrom);
         $to = DateTime::createFromFormat('Y-m-d\TH:i+', $dateTo);
         $days = $to->diff($from)->days;
-        $price = ($days + 1) * $eq->getPrice();
+        $price = ($days) * $eq->getPrice();
         $inquiry = array(
             'from' => $from,
             'to' => $to,
