d952ed1e0f7559a0f8a1b1f7cbe1cad03e35e77b
artur <artur@dantesoft.com>
2016-02-18 09:07:33 +0100

    talente: video url required

--- src/AppBundle/Controller/TalentController.php
+++ src/AppBundle/Controller/TalentController.php
@@ -367,9 +367,6 @@ class TalentController extends BaseController {
     }
     public function validateVideoUrl($value, ExecutionContextInterface $context) {
         $this->currentVideo = null;
-        if ($value == "") {
-            return;
-        }
         // vimeo
         preg_match(Video::RE_VIMEO, $value, $m);
         if (sizeof($m) !== 0) {
