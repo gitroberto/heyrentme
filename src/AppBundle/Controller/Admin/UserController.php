e298cf8cf154609393733e2757a4f97065360411
artur <artur@dantesoft.com>
2016-03-10 11:40:19 +0100

    backend: user add/edit

--- src/AppBundle/Controller/Admin/UserController.php
+++ src/AppBundle/Controller/Admin/UserController.php
@@ -3,12 +3,16 @@
 namespace AppBundle\Controller\Admin;
 
 use AppBundle\Entity\User;
-use Swift_Message;
-use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Swift_Message;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\ExecutionContextInterface;
 
 class UserController extends BaseAdminController {
      /**
@@ -43,7 +47,7 @@ class UserController extends BaseAdminController {
     
     /**
      * 
-     * @Route("/admin/users/details/{id}", name="admin_users_details")
+     * @Route("/admin/user/details/{id}", name="admin-user-details")
      */
     public function detailsAction(Request $request, $id) {
         $user = $this->getDoctrineRepo('AppBundle:User')->find($id);
@@ -90,6 +94,258 @@ class UserController extends BaseAdminController {
         ));
     }
     
+    /**
+     * @Route("/admin/user/add", name="admin-user-add")
+     */
+    public function addAction(Request $request) {
+        // build form
+        //<editor-fold>
+        $form = $this->createFormBuilder()
+            ->add('email', 'text', array(
+                'required' => false,
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 255))
+                )
+            ))
+            ->add('password', 'text', array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('min' => 6, 'max' => 255))
+                )
+            ))
+            ->add('name', 'text', array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 255))
+                )
+            ))
+            ->add('surname', 'text', array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 255))
+                )
+            ))
+            ->add('aboutMyself', 'text', array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 255))
+                )
+            ))
+            ->add('phone', 'text', array(
+                'required' => true,
+                'attr' => array(
+                    'maxlength' => 10, 
+                    'pattern' => '^[0-9]{1,10}$'),
+                'constraints' => array(
+                    new Regex(array('pattern' => '/^\d{1,10}$/', 'message' => 'Bitte gib hier eine gültige Telefonnummer ein'))
+                )
+            ))
+            ->add('phonePrefix', 'text', array(
+                'required' => true, 
+                'attr' => array('maxlength' => 3, 'pattern' => '^[0-9]{1,3}$'),
+                'constraints' => array(
+                    new Regex(array('pattern' => '/^\d{1,3}$/', 'message' => 'Bitte gib hier eine gültige Vorwahl ein'))
+                )
+            ))
+            ->add('iban', 'text', array(
+                'required' => false,
+                'constraints' => array(
+                    new Callback(array($this, 'validateIBAN'))
+                )
+            ))
+            ->add('bic', 'text', array(
+                'required' => false,
+                'constraints' => array(
+                    new Regex(array('pattern' => '/^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?$$/', 'message' => 'BIC-Code ist nicht korrekt.'))
+                )
+            ))
+            ->getForm();
+        //</editor-fold>
+        
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $data = $form->getData();
+                
+                $userMgr = $this->get('fos_user.user_manager');
+                $user = $userMgr->createUser();
+                $user->setUsername($data['email']);
+                $user->setEmail($data['email']);
+                $user->setPlainPassword($data['password']);
+                $user->setName($data['name']);
+                $user->setSurname($data['surname']);
+                $user->setPhonePrefix($data['phonePrefix']);
+                $user->setPhone($data['phone']);
+                $user->setIban($data['iban']);
+                $user->setBic($data['bic']);
+                $user->setAboutMyself($data['aboutMyself']);
+                $user->setEnabled(true);
+
+                $userMgr->updateCanonicalFields($user);                
+                $userMgr->updateUser($user);                
+                
+                return $this->redirectToRoute("admin_users_list");
+            }
+        
+        return $this->render('admin/user/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+    /**
+     * @Route("/admin/user/edit/{id}", name="admin-user-edit")
+     */
+    public function editAction(Request $request, $id) {
+        $user = $this->getDoctrineRepo('AppBundle:User')->find($id);
+        
+        $data = array(
+            'email' => $user->getEmail(),
+            'password' => '',
+            'name' => $user->getName(),
+            'surname' => $user->getSurname(),
+            'phonePrefix' => $user->getPhonePrefix(),
+            'phone' => $user->getPhone(),
+            'iban' => $user->getIban(),
+            'bic' => $user->getBic(),
+            'aboutMyself' => $user->getAboutMyself()
+        );
+        // build form
+        //<editor-fold>
+        $form = $this->createFormBuilder($data)
+            ->add('email', 'text', array(
+                'required' => false,
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 255))
+                )
+            ))
+            ->add('password', 'text', array(
+                'required' => false,
+                'constraints' => array(
+                    new Length(array('min' => 6, 'max' => 255))
+                )
+            ))
+            ->add('name', 'text', array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 255))
+                )
+            ))
+            ->add('surname', 'text', array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 255))
+                )
+            ))
+            ->add('aboutMyself', 'text', array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 255))
+                )
+            ))
+            ->add('phone', 'text', array(
+                'required' => true,
+                'attr' => array(
+                    'maxlength' => 10, 
+                    'pattern' => '^[0-9]{1,10}$'),
+                'constraints' => array(
+                    new Regex(array('pattern' => '/^\d{1,10}$/', 'message' => 'Bitte gib hier eine gültige Telefonnummer ein'))
+                )
+            ))
+            ->add('phonePrefix', 'text', array(
+                'required' => true, 
+                'attr' => array('maxlength' => 3, 'pattern' => '^[0-9]{1,3}$'),
+                'constraints' => array(
+                    new Regex(array('pattern' => '/^\d{1,3}$/', 'message' => 'Bitte gib hier eine gültige Vorwahl ein'))
+                )
+            ))
+            ->add('iban', 'text', array(
+                'required' => false,
+                'constraints' => array(
+                    new Callback(array($this, 'validateIBAN'))
+                )
+            ))
+            ->add('bic', 'text', array(
+                'required' => false,
+                'constraints' => array(
+                    new Regex(array('pattern' => '/^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?$$/', 'message' => 'BIC-Code ist nicht korrekt.'))
+                )
+            ))
+            ->getForm();
+        //</editor-fold>
+        
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $data = $form->getData();
+                
+                $userMgr = $this->get('fos_user.user_manager');
+                $user = $userMgr->createUser();
+                $user->setUsername($data['email']);
+                $user->setEmail($data['email']);
+                if (!empty($data['password'])) {
+                    $user->setPlainPassword($data['password']);
+                }
+                $user->setName($data['name']);
+                $user->setSurname($data['surname']);
+                $user->setPhonePrefix($data['phonePrefix']);
+                $user->setPhone($data['phone']);
+                $user->setIban($data['iban']);
+                $user->setBic($data['bic']);
+                $user->setAboutMyself($data['aboutMyself']);
+                $user->setEnabled(true);
+
+                $userMgr->updateCanonicalFields($user);                
+                $userMgr->updateUser($user);                
+                
+                return $this->redirectToRoute("admin_users_list");
+            }
+        
+        return $this->render('admin/user/edit.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+    
+    function validateIBAN($data, ExecutionContextInterface $context) {
+        $result = true;
+        
+        if ($data != null) {
+            $iban = $data;
+            
+            $iban = strtolower(str_replace(' ','',$iban));
+            $Countries = array('al'=>28,'ad'=>24,'at'=>20,'az'=>28,'bh'=>22,'be'=>16,'ba'=>20,'br'=>29,'bg'=>22,'cr'=>21,'hr'=>21,'cy'=>28,'cz'=>24,'dk'=>18,'do'=>28,'ee'=>20,'fo'=>18,'fi'=>18,'fr'=>27,'ge'=>22,'de'=>22,'gi'=>23,'gr'=>27,'gl'=>18,'gt'=>28,'hu'=>28,'is'=>26,'ie'=>22,'il'=>23,'it'=>27,'jo'=>30,'kz'=>20,'kw'=>30,'lv'=>21,'lb'=>28,'li'=>21,'lt'=>20,'lu'=>20,'mk'=>19,'mt'=>31,'mr'=>27,'mu'=>30,'mc'=>27,'md'=>24,'me'=>22,'nl'=>18,'no'=>15,'pk'=>24,'ps'=>29,'pl'=>28,'pt'=>25,'qa'=>29,'ro'=>24,'sm'=>27,'sa'=>24,'rs'=>22,'sk'=>24,'si'=>19,'es'=>24,'se'=>24,'ch'=>21,'tn'=>24,'tr'=>26,'ae'=>23,'gb'=>22,'vg'=>24);
+            $Chars = array('a'=>10,'b'=>11,'c'=>12,'d'=>13,'e'=>14,'f'=>15,'g'=>16,'h'=>17,'i'=>18,'j'=>19,'k'=>20,'l'=>21,'m'=>22,'n'=>23,'o'=>24,'p'=>25,'q'=>26,'r'=>27,'s'=>28,'t'=>29,'u'=>30,'v'=>31,'w'=>32,'x'=>33,'y'=>34,'z'=>35);
+
+            if (!array_key_exists(substr($iban,0,2), $Countries)) {
+                $result = false;
+            }
+            
+            if($result && strlen($iban) == $Countries[substr($iban,0,2)]){
+
+                $MovedChar = substr($iban, 4).substr($iban,0,4);
+                $MovedCharArray = str_split($MovedChar);
+                $NewString = "";
+
+                foreach($MovedCharArray AS $key => $value){
+                    if(!is_numeric($MovedCharArray[$key])){
+                        $MovedCharArray[$key] = $Chars[$MovedCharArray[$key]];
+                    }
+                    $NewString .= $MovedCharArray[$key];
+                }
+
+                if(bcmod($NewString, '97') != 1)
+                {                   
+                    $result = false;
+                }
+            }
+            else{
+                $result = false;
+            }   
+        }
+        
+        if (!$result) {
+            $context->buildViolation('IBAN number is incorrect.')
+                        ->atPath('iban')->addViolation();
+        }
+    }
     
     /**
      * @Route("/admin/users/jsondata", name="admin_users_jsondata")
