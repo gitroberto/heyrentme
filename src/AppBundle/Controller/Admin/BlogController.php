57208dadbea7006504760e3f34c576159324f1c4
artur <artur@dantesoft.com>
2016-05-31 10:03:08 +0200

    Neu bei: blogs, talents, equipments: blog showcase checkbox

--- src/AppBundle/Controller/Admin/BlogController.php
+++ src/AppBundle/Controller/Admin/BlogController.php
@@ -3,6 +3,7 @@ namespace AppBundle\Controller\Admin;
 
 use AppBundle\Entity\Blog;
 use AppBundle\Entity\BlogRelated;
+use AppBundle\Entity\Common;
 use AppBundle\Entity\Image;
 use AppBundle\Utils\Utils;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -514,6 +515,7 @@ class BlogController  extends BaseAdminController {
             $cell[4] = $dataRow->getModifiedAt()->format('Y-m-d H:i');
             $cell[5] = $dataRow->getPosition();
             $cell[6] = $this->generateUrl('blog_preview', array('uuid'=>$dataRow->getUuid()));
+            $cell[7] = $dataRow->getShowcase();
             
             $row['cell'] = $cell;
             array_push($rows, $row);
@@ -531,4 +533,29 @@ class BlogController  extends BaseAdminController {
         return $resp;
         
     }
+    /**
+     * @Route("admin/blog/showcase/{id}/{value}", name="admin-blog-showcase")
+     */
+    public function showcaseAction($id, $value) {
+        $repo = $this->getDoctrineRepo('AppBundle:Blog');
+        $post = $repo->find($id);
+        
+        // validate status
+        if ($value == 1 && !$post->getPublished())
+            return new JsonResponse(array('type' => 'error', 'message' => "The post is not public."));
+        
+        $count = $repo->getShowcaseStartCount();
+        if ($value == 1 && $count >= Common::BLOG_SHOWCASE_COUNT)
+            return new JsonResponse(array('type' => 'error', 'message' => "There are already ${count} posts selected"));
+                                
+        // set value & save
+        $post->setShowcase($value);
+        $this->getDoctrine()->getManager()->flush();
+                
+        // check for warning
+        $count = $repo->getShowcaseStartCount();        
+        $type = Common::BLOG_SHOWCASE_COUNT == $count ? "info" : "warning";
+        
+        return new JsonResponse(array('type' => $type, 'message' => "<strong>Start page</strong>: <strong>{$count}</strong> selected."));
+    }    
 }
