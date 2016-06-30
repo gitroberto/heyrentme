beccbf26982c9b28c2255bbf0c52887daaed6c83
artur <artur@dantesoft.com>
2016-06-30 17:04:52 +0200

    fix

--- app/Resources/sql/160628-tariff.sql
+++ app/Resources/sql/160628-tariff.sql
@@ -1,8 +1,12 @@
 
+
 ALTER TABLE `talent_inquiry`
-        CHANGE COLUMN `from_at` `from_at` DATETIME NULL AFTER `message`,
-	CHANGE COLUMN `to_at` `to_at` DATETIME NULL AFTER `from_at`,
-	ADD COLUMN `requestPrice` SMALLINT NULL AFTER `uuid`,
+        CHANGE COLUMN `from_at` `from_at` DATETIME NULL AFTER `message`;
+ALTER TABLE `talent_inquiry`
+	CHANGE COLUMN `to_at` `to_at` DATETIME NULL AFTER `from_at`;
+ALTER TABLE `talent_inquiry`
+	ADD COLUMN `request_price` SMALLINT NULL AFTER `uuid`;
+ALTER TABLE `talent_inquiry`
 	ADD COLUMN `num` INT NULL AFTER `request_price`;
 
 ALTER TABLE `talent_booking`
