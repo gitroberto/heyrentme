7f3f349147b2e223c3af55c904a2b383521caed8
artur <artur@dantesoft.com>
2016-09-16 16:20:11 +0200

    JS fix

--- web/js/heysharing.js
+++ web/js/heysharing.js
@@ -161,4 +161,29 @@ $(function(){
         };
         
         $("#formFeedback").submit(ajaxFormFeedbackSubmit);
+
+        var ajaxFormReportOfferSubmit = function () {
+            var $form = $(this);
+
+            var options = {
+                url: $form.attr("action"),
+                type: $form.attr("method"),
+                data: $form.serialize()
+            };
+
+            $.ajax(options).done(function (data) {    
+                var $target = $("#formReportOffer");
+                var $newHtml = $(data);        
+                
+                $target.replaceWith($newHtml);
+                if (data.indexOf("Report_Offer_Saved") != -1){                    
+                    $("#ReportOfferConfirmationDiv").show();                   
+                }
+            });
+
+            return false;
+            
+        };
+
+        $("#formReportOffer").submit(ajaxFormReportOfferSubmit);
 });
